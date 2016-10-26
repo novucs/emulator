d4b245c99d102858ff8d3ec87a9fcc68b5c9e78e
novucs <contact@novucs.net>
2016-10-26 17:48:35 +0100

    Implement DECA

--- main.c
+++ main.c
@@ -1018,6 +1018,13 @@ void Group_1(BYTE opcode) {
       set_flag_z(Registers[REGISTER_B]);
       set_flag_n(Registers[REGISTER_B]);
       break;
+
+    // DECA - Decrement accumulator
+    case 0xD1:
+      Registers[REGISTER_A]--;
+      set_flag_z(Registers[REGISTER_A]);
+      set_flag_n(Registers[REGISTER_A]);
+      break;
   }
 }
 
